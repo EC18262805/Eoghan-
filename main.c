20bb2e21fd00d938cf8b9997076502fc244ad7f7
EC18262805 <58124879+EC18262805@users.noreply.github.com>
2019-11-25 04:43:56 -0800

    Add files via upload

--- main.c
+++ main.c
@@ -12,13 +12,30 @@ double main()
 
 }
 
-void store
+typedef struct pollys
 {
-    typedef struct
-    {
+double coeff [500];//the coefficient of the polly
+double exponent [500];//the exponent of the polly
+enum Operator {1,2,3,4,5};
+//1=add,2=minus,3=multiply,4=divide,5=none(used for last polly only)
+}
+poly;
+
+void double createPolly
+{
+/*
+creates varibles to store the pollys values
+asks the user for the varibles of the polly
+scans the coefficient from the user
+scans the exponent from the user
+scans the operator from the user
+saves the values to an array of pollys
+*/
+double tempCeoff;
+double tempEx;
+int tempOp;
+printf("Input the value of the pollynomials coefficient\n")
+scanf()
 
 
-
-    }
-    poly;
 }
