dbc0b90333aa5e2c99f525e543045cbdb0241c39
EC18262805 <58124879+EC18262805@users.noreply.github.com>
2019-11-27 11:01:59 -0800

    Add files via upload

--- functions.c
+++ functions.c
@@ -1,94 +1,38 @@
-#include <stdlib.h>
-#include <stdio.h>
+/*
 
-struct polly
+
+*/
+mulPoly()
 {
-int coeff;
-};
+    double  mul;
+    printf("Type the vaule you want the polynomial multipied by\n");
+    scanf("%lf", &mul);
 
-int addPoly(struct poly pa1[10], struct poly pa2[10], int t1, int t2, struct poly pa[10])
-{
-    int choice1;
-    int choice2;
-    printf("select the two polynomials you want added togrther\nselect the first polynomial");
-    sacnf("%d", &choice1);
-    printf("select the second polynomial\n");
-    scanf("%d", &choice2);
-    
-    //pa(choice1)[]==pa1[];
-    //pa(choice2)[]==pa2[];
-    
-    int i=0;
-    int j=0;
-    int k=0;
-
-    while(i<t1 && j<t2)
+    for(int i=0;i<maxOrder;i++)
     {
-        if(pa1[i].expo==pa2[j].expo)
-        {
-            pa[k].coeff=pa1[i].coeff + pa2[j].coeff;
-
-            i++;
-            j++;
-            k++;
-        }
-        else if(pa1[i].expo>pa2[j].expo)
-        {
-            pa[k].coeff=pa1[i].coeff;
-            i++;
-            k++;
-        }
-        else
-        {
-        pa[k].coeff=pa2[j].coeff;
-        j++;
-        k++;
-        }
+    p[i].coeff=mul*p[i].ceoff;
     }
-    while(i<t1)
-    {
-        pa[k].coeff=pa1[i].coeff;
-        pa[k].coeff=pa1[i].expo;
-        i++;
-        k++;
-    }
-    while(j<t2)
-    {
-        pa[k].coeff=pa2[j].coeff;
-        pa[k].coeff=pa2[j].expo;
-        j++;
-        k++;
-    }
-
-    return(k);
 
 }
 
-
-int readPoly(struct poly p[10])
+divPoly()
 {
-    int t1,i;
+    double  div;
+    printf("Type the vaule you want the polynomial divided by\n");
+    scanf("%lf", &div);
 
-    printf("Highest Degree of the Polynomial: \n");
-    scanf("%d", &t1);
-
-    printf("Enter the Coefficients in Ascending order\n");
-    for(i=0;i<t1;i++)
+    for(int i=0;i<maxOrder;i++)
     {
-        printf("Enter the Coefficient(x^%d):\n", i);
-        scanf("%d", &p[i].coeff);
-        //printf("Enter the coresponding exponent(%d):",i+1);
-        //scanf("%d,&p[i].exponent);
+    p[i].coeff=(p[i].ceoff)/(div);
     }
-    return t1;
+
 }
 
-void displayPoly(struct poly p[10], int term)
+normPoly()
 {
-    int k;
-
-    for(k=0;k<term-1;k++)
-    if(p[k].coeff==0)
-    printf("%d(x^%d)+", p[k].coeff, k);
-    printf("%d(x^%d)", p[term-1].coeff,k);
+int norm = p[maxOrder].coeff;
+    for(int i=0;i<maxOrder;i++)
+    {
+    p[i].coeff=()p[i].ceoff)/(norm);
+    }
 }
