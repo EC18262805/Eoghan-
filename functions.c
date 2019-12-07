685f3dc5a76bb9c398ff9ca62436fccf226ce462
EC18262805 <58124879+EC18262805@users.noreply.github.com>
2019-12-07 07:13:29 -0800

    Update functions.c

--- functions.c
+++ functions.c
@@ -1,38 +1,133 @@
-/*
+//authors: Eoghan Campion, Fionn Murray, Ciaran Moroney
+//date: 24/11/19
+#include<stdio.h>
+#include<malloc.h>
+
+struct link{
+       double coeff;
+       int pow;
+       struct link *next;
+       };
 
 
-*/
-mulPoly()
+struct link *poly2=NULL,*polyDiv=NULL, *polyDivCorrect=NULL;
+
+void divPoly(struct link *polyDiv, struct link *poly1);
+void createPolyDivCorrect(struct link *node);
+void display(struct link *node);
+
+void createPoly2(struct link *node)
 {
-    double  mul;
-    printf("Type the vaule you want the polynomial multipied by\n");
-    scanf("%lf", &mul);
 
-    for(int i=0;i<maxOrder;i++)
+    node->coeff =4;
+    node->pow = 0;
+    node->next=(struct link *)malloc(sizeof(struct link));
+    node=node->next;
+    node->next=NULL;
+    node->coeff = 5;
+    node->pow = 1;
+    node->next=(struct link *)malloc(sizeof(struct link));
+    node=node->next;
+    node->next=NULL;
+    node->coeff = 1;
+    node->pow = 2;
+    node->next=(struct link *)malloc(sizeof(struct link));
+    node=node->next;
+    node->next=NULL;
+    node->coeff = 2;
+    node->pow = 3;
+    node->next=(struct link *)malloc(sizeof(struct link));
+    node=node->next;
+    node->next=NULL;
+}
+
+
+
+void display(struct link *node)
+{
+ while(node->next!=NULL)
+ {
+  printf("%fx^%d",node->coeff,node->pow);
+  node=node->next;
+  if(node->next!=NULL)
+   printf(" + ");
+ }
+}
+
+int main()
+{
+
+    poly2=(struct link *)malloc(sizeof(struct link));
+    polyDiv=(struct link *)malloc(sizeof(struct link));
+    polyDivCorrect=(struct link *)malloc(sizeof(struct link));
+
+    createPoly2(poly2);
+    printf("\n\nFirst polynomial:");
+    display(poly2);
+
+    divPoly(polyDiv,poly2);
+    printf("\n\nmultiplied Polynomial:");
+    display(polyDiv);
+
+    createPolyDivCorrect(polyDivCorrect);
+    printf("\n\nCorrect Answer:");
+    display(polyDivCorrect);
+
+    if(polyDiv->coeff==polyDivCorrect->coeff && polyDiv->pow==polyDivCorrect->pow)
     {
-    p[i].coeff=mul*p[i].ceoff;
+        printf("\n\nTest Passed");
     }
+    else
+    {
+        printf("\n\nTest Failed");
+    }
+}
+
+
+void divPoly(struct link *poly2, struct link *polyDiv)
+{
+    double div = 2;
+    double temp;
+    int temp2;
+
+    while(poly2->next)
+        {
+        temp = poly2->coeff/div;
+        temp2 = poly2->pow;
+
+        poly2=poly2->next;
+
+        polyDiv->coeff = temp;
+        polyDiv->pow = temp2;
+
+        polyDiv->next = (struct link*)malloc(sizeof(struct link));
+        polyDiv=polyDiv->next;
+        polyDiv->next = NULL;
+
+        }
 
 }
 
-divPoly()
+void createPolyDivCorrect(struct link *node)
 {
-    double  div;
-    printf("Type the vaule you want the polynomial divided by\n");
-    scanf("%lf", &div);
-
-    for(int i=0;i<maxOrder;i++)
-    {
-    p[i].coeff=(p[i].ceoff)/(div);
-    }
-
-}
-
-normPoly()
-{
-int norm = p[maxOrder].coeff;
-    for(int i=0;i<maxOrder;i++)
-    {
-    p[i].coeff=()p[i].ceoff)/(norm);
-    }
+    node->coeff = 2;
+    node->pow = 0;
+    node->next=(struct link *)malloc(sizeof(struct link));
+    node=node->next;
+    node->next=NULL;
+    node->coeff = 2.5;
+    node->pow = 1;
+    node->next=(struct link *)malloc(sizeof(struct link));
+    node=node->next;
+    node->next=NULL;
+    node->coeff = 0.5;
+    node->pow = 2;
+    node->next=(struct link *)malloc(sizeof(struct link));
+    node=node->next;
+    node->next=NULL;
+    node->coeff = 1.5;
+    node->pow = 3;
+    node->next=(struct link *)malloc(sizeof(struct link));
+    node=node->next;
+    node->next=NULL;
 }
